f8626336f02487bc0c8b4f729a9b43243cdcfdf8
Алексей Тен <virtuoso_ten@mail.ru>
2024-01-16 22:54:09 +0300

    Добавил новое поле и применил миграции и зафиксировал их

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
     name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
