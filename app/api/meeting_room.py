1a6b19762530d5cc9cc1421246a27cdb03bfb452
aten88 <virtuoso_ten@mail.ru>
2024-01-18 18:21:05 +0300

    Внедрил pydantic схему ответов на запросы

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -3,12 +3,16 @@
 from fastapi import APIRouter, HTTPException
 
 from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
-from app.schemas.meeting_room import MeetingRoomCreate
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
 
 router = APIRouter()
 
 
-@router.post('/meeting_rooms/')
+@router.post(
+        '/meeting_rooms/',
+        response_model=MeetingRoomDB,
+        response_model_exclude_none=True,
+    )
 async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate,
 ):
--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -3,12 +3,24 @@ from typing import Optional
 from pydantic import BaseModel, Field, validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., max_length=100)
+class MeetingRoomBase(BaseModel):
+    name: str = Field(None, min_length=1, max_length=100)
 
     @validator('name')
     def name_is_not_empty(cls, value):
         if not value.strip():
             raise ValueError('Поле name не может быть пустым.')
         return value
+
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomDB(MeetingRoomBase):
+    id: int
+
+    class Config:
+        orm_mode = True
