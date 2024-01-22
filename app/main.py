1e324632ce07faf8fc962e8b235d9cb63dd7f234
Алексей Тен <virtuoso_ten@mail.ru>
2024-01-22 20:48:10 +0300

    Реализовал автоматическое создание суперюзера

--- app/main.py
+++ app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
+from app.core import create_first_superuser
 
 app = FastAPI(title=settings.app_title)
 
 app.include_router(main_router)
+
+
+@app.on_event('startup')
+async def startup():
+    await create_first_superuser()
