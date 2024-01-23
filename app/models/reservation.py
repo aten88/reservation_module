820b0b7a92ef2e43f6be76e31920cbc2400429fe
aten88 <virtuoso_ten@mail.ru>
2024-01-23 13:32:01 +0300

    Правка по PEP 8

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -7,7 +7,9 @@ class Reservation(Base):
     from_reserve = Column(DateTime)
     to_reserve = Column(DateTime)
     meetingroom_id = Column(Integer, ForeignKey('meetingroom.id'))
-    user_id = Column(Integer, ForeignKey('user.id', name='fk_reservation_user_id_user'))
+    user_id = Column(Integer, ForeignKey(
+        'user.id', name='fk_reservation_user_id_user'
+    ))
 
     def __repr__(self) -> str:
         return (
